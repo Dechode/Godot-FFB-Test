0b4f1ab22aca2b7e06da37cbec4853c5b16e3581
Veeti Väisänen <veetiv.aisanen1@gmail.com>
2022-02-10 21:18:54 +0200

    Bugfixes

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -14,13 +14,8 @@ var amplitude: float = 0.0
 func _ready() -> void:
 	$ffbplugin.init_ffb(0) 
 	is_ffb = $ffbplugin.force_feedback
-#	if $ffbplugin.init_constant_force_effect() != 0:
-#		print("Error initialising constant force effect")
-##		$ffbplugin.init_constant_force_effect()
-#	else:
-#		print("Constant force effect initialised")
 		
-	print("Has constant force effect = ",is_ffb)
+	print("Has force feedback = ",is_ffb)
 	
 	$AmplitudeSlider.value = amplitude
 
@@ -31,21 +26,12 @@ func _process(delta: float) -> void:
 
 	var ffb_signal = $AmplitudeSlider.value
 	
-#	if is_running and is_ffb:
 	if is_running:
 		print(ffb_signal)
-#		print($ffbplugin.cf_id)
-		if $ffbplugin.update_constant_ffb_effect(ffb_signal, 0) != 0:
+		if $ffbplugin.update_constant_force_effect(ffb_signal, 0) != 0:
 			print("Updating constant force effect failed!")
-#		$ffbplugin.update_constant_ffb_effect(ffb_signal)
 	else:
 		$ffbplugin.destroy_ffb_effect(0)
-#		if is_ffb:
-#			if $ffbplugin.update_constant_ffb_effect(0.0) != 0:
-##			$ffbplugin.update_constant_ffb_effect(0.0)
-#				print("Updating constant force effect failed!")
-#		else:
-#			print("No force feedback device found")
 
 
 func _on_AmplitudeSlider_value_changed(value: float) -> void:
@@ -55,13 +41,14 @@ func _on_AmplitudeSlider_value_changed(value: float) -> void:
 func _on_StartBtn_pressed() -> void:
 	if $ffbplugin.init_constant_force_effect() != 0:
 		print("Error initialising constant force effect")
-	if $ffbplugin.play_constant_ffb_effect(-1, 0) != 0:
+	if $ffbplugin.play_constant_force_effect(0, 0) != 0:
 		print("Starting ffb effect failed")
 	is_running = true
 
 
 func _on_StopBtn_pressed() -> void:
 	is_running = false
-	$ffbplugin.destroy_ffb_effect(-1)
+#	$ffbplugin.destroy_ffb_effect(-1)
+	$ffbplugin.destroy_ffb_effect(2)
 	
 
