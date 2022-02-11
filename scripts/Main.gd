cb9adf94e26f604ed90f6f2e148deb8b6e0842ad
Veeti Väisänen <veetiv.aisanen1@gmail.com>
2022-02-11 02:20:37 +0200

    Updated ffb plugin

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -1,8 +1,5 @@
 extends Control
 
-
-export (Curve) var ffb_signal_wave = null
-
 var is_ffb: bool = false
 var is_running: bool = false
 var effect_id = -1
@@ -12,43 +9,47 @@ var amplitude: float = 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$ffbplugin.init_ffb(0) 
+	var err = $ffbplugin.init_ffb(0) # Initializes the haptic subsystem for given device id
+	if err < 0:
+		print("Cant initialize haptic subsystem, most likely device not haptic")
 	is_ffb = $ffbplugin.force_feedback
 		
 	print("Has force feedback = ",is_ffb)
 	
 	$AmplitudeSlider.value = amplitude
-
-
-
+	$CurrentValue.text = amplitude as String
+	
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 
 	var ffb_signal = $AmplitudeSlider.value
 	
 	if is_running:
-		print(ffb_signal)
+#		print(ffb_signal)
 		if $ffbplugin.update_constant_force_effect(ffb_signal, 0) != 0:
 			print("Updating constant force effect failed!")
-	else:
-		$ffbplugin.destroy_ffb_effect(0)
+#	else:
+#		$ffbplugin.destroy_ffb_effect(effect_id)
 
 
 func _on_AmplitudeSlider_value_changed(value: float) -> void:
 	amplitude = value
+	$CurrentValue.text = value as String
 
 
 func _on_StartBtn_pressed() -> void:
-	if $ffbplugin.init_constant_force_effect() != 0:
+	effect_id = $ffbplugin.init_constant_force_effect() # Initializes constant force effect and returns its effect_id
+	if effect_id < 0:
 		print("Error initialising constant force effect")
-	if $ffbplugin.play_constant_force_effect(0, 0) != 0:
+	if $ffbplugin.play_constant_force_effect(effect_id, 0) < 0:
 		print("Starting ffb effect failed")
 	is_running = true
 
 
 func _on_StopBtn_pressed() -> void:
 	is_running = false
-#	$ffbplugin.destroy_ffb_effect(-1)
-	$ffbplugin.destroy_ffb_effect(2)
+	$ffbplugin.destroy_ffb_effect(effect_id)
+	effect_id = -1
 	
 
